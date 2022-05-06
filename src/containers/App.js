86eb64094addb81812752caebc20b20d87ae5df6
Bri'Anna Tolliver <bri39annat@gmail.com>
2022-05-06 15:49:23 -0500

    put robo app on git

---
+++ src/containers/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
+import './App.css';
+
+class App extends Component {
+	constructor(){
+		super()
+		this.state= {
+				robots: [],
+				searchfield: ''
+		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({ robots: users }))
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+	}
+
+	render(){
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		});
+
+		return !robots.length ?
+		 <h1>Loading..</h1> :
+			 (
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange} />
+					<Scroll>
+						<ErrorBoundary>
+							<CardList robots={filteredRobots}/>
+						</ErrorBoundary>
+					</Scroll>
+				</div>
+			);
+	}
+}
+
+export default App;
+
+//state is the description of your app; it's an object
+//props are things that come out of state
+//state >> props; parent feeds state into child component and it receives it as unchangeble property
